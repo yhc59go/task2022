68fef684a533c75f6a28cc4b110c206385b5884e
Christine <yhc59go@gmail.com>
2022-10-04 23:48:08 +0800

    [DEV] Add assignment 3-1
    Add assignment 3-1.

---
+++ week-3/assignment_3-1.py
@@ -0,0 +1,29 @@
+from unittest import result
+import urllib.request as request
+import json
+import re
+import csv
+src="https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment.json"
+with request.urlopen(src)as response:
+    data=json.load(response)
+    
+csvPath="C:\\Users\\YHC\\Documents\\Study\\WeHelp Bootcamp\\week1_0919\\github\\task2022\\week-3\\"
+with open(csvPath+'data.csv','w', newline='', encoding="utf-8") as csvFile:
+    writer=csv.writer(csvFile)
+    
+    for idx in range(0,len(data["result"]["results"])):
+        yearOfData=data["result"]["results"][idx]["xpostDate"].split("/")
+        if int(yearOfData[0])>=2015:
+            administrativeRegion=re.search(r'\s.*區' , data["result"]["results"][idx]["address"] ).group(0).strip()
+            imageFile=re.split("jpg",data["result"]["results"][idx]["file"], flags=re.IGNORECASE)       
+            writer.writerow(
+                #景點名稱,區域,經度,緯度,第一張圖檔網址
+                [data["result"]["results"][idx]["stitle"],
+                administrativeRegion,
+                data["result"]["results"][idx]["longitude"],
+                data["result"]["results"][idx]["latitude"],
+                imageFile[0]+"jpg"])
+  
+
+
+
